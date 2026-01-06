837a543c1ea8a72475aa387a9821efcc24be78e2
nawaphat06 <tantan.nawaphat@gmail.com>
2026-01-06 10:47:56 +0700

    lab12_2 complete

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 
+long gcd(long x, long y);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+long gcd(long x, long y){
+    if(x % y == 0) return y;
+    else return gcd(y, x%y);
 }
